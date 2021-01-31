0722c2911e53298bdae17365a511c36867db600e
opeshamz <Samxyn007@yahoo.com>
2021-01-31 21:23:14 +0100

    Update validation.js

--- routes/validation.js
+++ routes/validation.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     res.status(500).json({ message: err.message })
   }
 })
-// Creating one
+// Creating on
 router.post('/', (req, res) => {
 
   const scrams = {
